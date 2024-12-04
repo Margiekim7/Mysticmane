f7a1283a32523cc694d695c28080099d71cb3a2e
Margiekim7 <margaretkimani987@gmail.com>
2024-12-04 08:52:10 +0300

    made some changes

--- mysticapp/urls.py
+++ mysticapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blog/', views.index, name='blog'),
-    path('singleblog/', views.index, name='single_blog'),
-    path('contact/', views.index, name='contact'),
-    path('element/', views.index, name='elements'),
-    path('about/', views.index, name='about'),
-    path('service/', views.index, name='service'),
+    path('blog/', views.blog, name='blog'),
+    path('singleblog/', views.singleblog, name='single_blog'),
+    path('contact/', views.contact, name='contact'),
+    path('element/', views.elements, name='elements'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
 ]
