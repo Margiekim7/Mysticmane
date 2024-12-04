6033ae9fb1c937fc7ecce3cce8000f1bdfc233e4
Margiekim7 <margaretkimani987@gmail.com>
2024-12-04 16:09:09 +0300

    mini changes

--- mysticapp/urls.py
+++ mysticapp/urls.py
@@ -6,13 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog/', views.blog, name='blog'),
-    path('singleblog/', views.singleblog, name='single_blog'),
-
-    path('element/', views.elements, name='elements'),
     path('about/', views.about, name='about'),
     path('service/', views.service, name='service'),
     path('pricing/', views.pricing, name='pricing'),
     path('appointment/', views.appointment, name='appointment'),
-
+    path('contact/', views.contact, name='contact'),
 
 ]
--- mysticapp/views.py
+++ mysticapp/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-def blog(request):
-    return render(request,'blog.html')
-def singleblog(request):
-    return render(request,'single-blog.html')
 
-def elements(request):
-    return render(request,'elements.html')
 def about(request):
     return render(request,'about.html')
 def service(request):
@@ -17,6 +11,12 @@ def service(request):
 def pricing(request):
     return render(request, 'pricing.html')
 def appointment(request):
-    return render(request, 'appointment')
+    return render(request, 'appointment.html')
 
+def contact(request):
+    return render(request, 'contact.html')
+def blog(request):
+    return render(request, 'blog.html')
 
+def contact(request):
+    return render(request, 'contact.html')
